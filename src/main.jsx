6f38addde7f39c95fa7264c379f37ad83233c326
APZN <129354802+jsk1004ha@users.noreply.github.com>
2026-06-30 22:43:54 +0900

    Render app without Supabase bridge

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { initializeCloudStorageBridge } from "./services/cloudStorageBridge.js";
 import "./styles.css";
 
-async function bootstrap() {
-  await initializeCloudStorageBridge();
-  createRoot(document.getElementById("root")).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
-
-bootstrap();
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
